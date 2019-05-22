cd7c322a3899a57c4b2c7299b137cd03e3a43359
hyeongyeong <kohy0329@naver.com>
2019-05-22 18:05:27 +0900

    test17144

--- src/test17144/Main.java
+++ src/test17144/Main.java
@@ -34,7 +34,7 @@ public class Main {
 		
 		for(int y = 0; y < C ; y ++) {
 			for (int x = 0; x <R ; x ++ ) {
-				if(map[y][x] > -1)
+				if(map[y][x] > 0)
 					cnt += map[y][x];
 			}
 		}
@@ -43,34 +43,87 @@ public class Main {
 	}
 	
 	public static void mapCopy(int src[][], int dest[][]) {
-		for(int i=0; i<C; i++) {
-            for(int j=0; j<R; j++) {
-                dest[i][j] = src[i][j];
+		for(int y=0; y<C; y++) {
+            for(int x=0; x<R; x++) {
+                dest[y][x] = src[y][x];
             }
-        }
+        } 
 	}
+	
+//	public static void printMap(int src[][] ) {
+//		for(int y=0; y<C; y++) {
+//            for(int x=0; x<R; x++) {
+//               System.out.print(src[y][x] + " ");
+//            }
+//            System.out.println();
+//        } 
+//	}
 	
 	public static void expansion(int x, int y) {
 		
 		int tempX, tempY;
 		
-		if(map[y][x] > -1) {
+		if(map[y][x] > 0) {
 			for(int i=0;i<4;i++) {
 				tempX = x + dx[i];
 				tempY = y + dy[i];
 				
-				if(-1<tempX && tempX<C && -1<tempY && tempY <R) {
+				if(-1<tempX && tempX<R && -1<tempY && tempY <C && map[tempY][tempX] != -1) {
 					copiedMap[tempY][tempX] += map[y][x]/5;
 					copiedMap[y][x] -= map[y][x]/5;
-					expansion(tempX,tempY);
 				}
 				
 			}
 		}
 	}
 	
-	public static void clean(int direction, int x, int y) { // 공기청정기 2개, 반시계, 시계방향으로 동작.
+	public static void cycleClean(int index, int x, int y, boolean fig) { // 시계방향으로 동작.
 		
+		
+		if(x == cleaner.get(index).left && y== cleaner.get(index).right && fig) {
+			return;	
+		} 
+		if(map[y][x] == -1) {
+			copiedMap[y][x+1] = 0;
+			cycleClean(index,x+1,y, true);
+		} else if(y== cleaner.get(index).right && x<R-1 ) { // 오른쪽으로 이동 
+			copiedMap[y][x+1] = map[y][x];
+			cycleClean(index,x+1,y, fig);
+		} else if(x==R-1 && y < C-1) { // 아래로 이동 
+			copiedMap[y+1][x] = map[y][x];
+			cycleClean(index,x,y+1, fig);
+		} else if(x>0 && y ==C-1) { // 왼쪽으로 이동 
+			copiedMap[y][x-1]=map[y][x];
+			cycleClean(index,x-1,y, fig);
+		} else if(x==0 && y>cleaner.get(index).right+1) { // 위로 이
+			copiedMap[y-1][x]=map[y][x];
+			cycleClean(index,x,y-1, fig);
+		}
+	}
+	
+	public static void acycleClean(int index, int x, int y, boolean fig) { // 반시계방향으로 동작.
+		
+		
+		if(x == cleaner.get(index).left && y== cleaner.get(index).right&& fig) {
+			return;
+		}
+		
+		if(map[y][x] == -1) {
+			copiedMap[y][x+1] = 0;
+			acycleClean(index,x+1,y, true);
+		} else if ( x < R-1 && y == cleaner.get(index).right ) { // 오른쪽으로 이동 
+			copiedMap[y][x+1] = map[y][x];
+			acycleClean(index, x+1, y,fig);
+		} else if (y > 0 && x == R-1) { // 위로 이동 
+			copiedMap[y-1][x] = map[y][x];
+			acycleClean(index,x,y-1, fig);
+		} else if (y==0 && x>0) { // 왼쪽으로 이동 
+			copiedMap[y][x-1] = map[y][x];
+			acycleClean(index,x-1,y, fig);
+		} else if( x==0 && y < cleaner.get(index).right-1){ // 아래로 이동 
+			copiedMap[y+1][x] = map[y][x];
+			acycleClean(index,x,y+1, fig);
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -79,11 +132,13 @@ public class Main {
 		int answer = 0;
 		Scanner scan = new Scanner(System.in);
 		
-		R = scan.nextInt();
 		C = scan.nextInt();
+		R = scan.nextInt();
 		T = scan.nextInt();
 		
 		map = new int[C][R];
+		copiedMap = new int[C][R];
+		
 		for(int y = 0 ; y<C ; y ++) {
 			for(int x = 0 ; x<R ; x++) {
 				map[y][x] = scan.nextInt();
@@ -102,10 +157,15 @@ public class Main {
 					expansion(x,y);
 				}
 			}
-			
-			clean(-1, cleaner.get(0).left,cleaner.get(0).right); // 반시계 회전 
-			clean(1, cleaner.get(1).left,cleaner.get(1).right); // 시계 회전 
 			mapCopy(copiedMap, map);
+//			printMap(map);
+			acycleClean(0, cleaner.get(0).left,cleaner.get(0).right, false); // 반시계 회전 
+			cycleClean(1, cleaner.get(1).left,cleaner.get(1).right, false); // 시계 회전 
+			mapCopy(copiedMap, map);
+			
+//			System.out.println(T);
+//			printMap(map);
+			
 			
 		}
 		
